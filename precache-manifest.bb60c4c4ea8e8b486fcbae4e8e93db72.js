8adebf05617ab0dc3eee5deeb311d0f3f1dc86ee
ugo070 <ugo070@yahoo.com>
2020-01-21 17:36:29 -0500

    Updates

---
+++ precache-manifest.bb60c4c4ea8e8b486fcbae4e8e93db72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e976a2d8d949b1fae28a0573fea401b",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "1ee65f541e09814fb2d0",
+    "url": "/ClickyGame/static/css/main.b0598d68.chunk.css"
+  },
+  {
+    "revision": "2dc69e46894faafc50a7",
+    "url": "/ClickyGame/static/js/2.89661180.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.89661180.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ee65f541e09814fb2d0",
+    "url": "/ClickyGame/static/js/main.b23fd1b4.chunk.js"
+  },
+  {
+    "revision": "c5ff570fb451c3db5c59",
+    "url": "/ClickyGame/static/js/runtime-main.3a261c6c.js"
+  }
+]);
